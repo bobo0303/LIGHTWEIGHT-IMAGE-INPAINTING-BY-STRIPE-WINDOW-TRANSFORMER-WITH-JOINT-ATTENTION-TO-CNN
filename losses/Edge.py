dd9b535ba295bedae7b294e915b7df89f51b7080
BO-WEI, CHEN <56902242+bobo0303@users.noreply.github.com>
2023-08-21 22:34:07 +0800

    Delete losses directory

--- losses/Edge.py
+++
@@ -1,8 +0,0 @@
-import torch.nn.functional as F
-
-def Edge_MSE_loss(Original_img, pred_img, Edge):
-
-    loss = F.mse_loss(Original_img, pred_img, reduction='none')
-    loss = loss * (1 - Edge) + loss * Edge * 10
-    return loss.mean()
-
